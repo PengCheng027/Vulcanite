7e8743631b5d58583b7ce4a5294281c3a93f96b8
PengCheng <PengCheng277@163.com>
2026-08-26 23:07:41 +0800

    Migrate to CMake, integrate spdlog v1.17.0 as submodule

--- Vulcanite/src/main.cpp
+++ Vulcanite/src/main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 int main() {
-	std::cout << "hello vulcanite engine";
+	// 彩色终端日志器
+	auto console = spdlog::stdout_color_mt("console");
+
+	console->info("hello vulcanite engine");
+	console->warn("this is a warning");
+	console->error("this is an error");
+
+	spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
+
+	console->info("colored pattern test");
+
+	return 0;
 }
